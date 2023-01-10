d91125eb82e3b1c24c0559ba7dbc0f5eb37393de
Susan Kronberg <id19skg@cs.umu.se>
2023-01-10 17:16:25 +0100

    functionality for tags in ads

--- src/components/ads/Request.js
+++ src/components/ads/Request.js
@@ -1,23 +1,26 @@
 import React from 'react';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { db } from '../../firebase';
 import { useAuth } from '../../context/AuthContext';
 import { useNavigate } from 'react-router-dom';
 import { doc, setDoc } from 'firebase/firestore';
 import { collection, addDoc } from 'firebase/firestore';
 import Heading1 from '../textComponents/Heading1';
-
+import TagSearch from '../atomics/TagSearch';
+import { searchableTags } from '../../api/SearchAPI';
+import TagSection from '../atomics/TagsSection';
 export default function Request() {
   const { currentUser } = useAuth();
-
   const [titel, setTitel] = useState('');
   const [description, setDescription] = useState('');
-  const [tags, setTags] = useState('');
+  const [tags, setTags] = useState([]);
   const [imageUrl, setImageUrl] = useState('');
   const [place, setPlace] = useState('');
   const [compensation, setCompensation] = useState('');
   const [time, setTime] = useState('');
   const [date, setDate] = useState('');
+  const [displayTags, setDisplayTags] = useState([]);
+  const [userTags, setUserTags] = useState([]);
 
   const uid = currentUser.uid;
 
@@ -25,7 +28,9 @@ export default function Request() {
 
   const handleSubmit = (event) => {
     event.preventDefault();
-
+    console.log(String(date))
+    console.log(userTags)
+    
     addDoc(collection(db, 'adRequest'), {
       titel: titel,
       description: description,
@@ -40,14 +45,24 @@ export default function Request() {
 
     setTitel('');
     setDescription('');
-    setTags('');
+    setTags([]);
     setPlace('');
     setCompensation('');
     setTime('');
     setDate('');
-
     navigate('/home');
   };
+
+  useEffect(() => {
+    searchableTags();
+  }, []);
+
+  const handleUserTags = (childData) =>{
+    setUserTags([...userTags, childData])
+}
+const handleDisplayTags = (childData) =>{
+  setDisplayTags([...displayTags, childData]);
+}
 
   return (
     <>
@@ -80,17 +95,6 @@ export default function Request() {
             <input
               className="inputField"
               type="text"
-              id="tags"
-              name="tags"
-              value={tags}
-              placeholder="Taggar"
-              onChange={(event) => setTags(event.target.value)}
-            />
-          </div>
-          <div className="formGroup">
-            <input
-              className="inputField"
-              type="text"
               id="imageUrl"
               name="imageUrl"
               value={imageUrl}
@@ -112,7 +116,7 @@ export default function Request() {
           <div className="formGroup">
             <input
               className="inputField"
-              type="text"
+              type="time"
               id="time"
               name="time"
               value={time}
@@ -122,13 +126,13 @@ export default function Request() {
           </div>
           <div className="formGroup">
             <input
-              className="inputField"
-              type="text"
+            className="inputField"
+              type="date"
               id="date"
               name="date"
               value={date}
               placeholder="Datum"
-              onChange={(event) => setTime(event.target.value)}
+              onChange={(event) => setDate(event.target.value)}
             />
           </div>
           <div className="formGroup">
@@ -142,7 +146,22 @@ export default function Request() {
               onChange={(event) => setCompensation(event.target.value)}
             />
           </div>
+          <div className="formGroup">
+              <TagSearch 
+              passUserTags={handleUserTags}
+              passDisplayTags={handleDisplayTags}
+              userTags={userTags}/>
+              
+              <TagSection
+                  mini={true}
+                  primary={false}
+                  sectionTitle={'Valda taggar'}
+                  tags={displayTags}
+                ></TagSection>
+          </div>
 
+
+    
           <input
             //disabled={loading}
             className="button"
--- src/components/login/SelectTags.js
+++ src/components/login/SelectTags.js
@@ -16,6 +16,7 @@ import Button from '../atomics/Button';
 import { doc, setDoc, updateDoc } from 'firebase/firestore';
 import { db } from '../../firebase';
 import { useAuth } from '../../context/AuthContext';
+import TagSearch from '../atomics/TagSearch';
 
 function SelectTags() {
   const [displayTags, setDisplayTags] = useState([]);
@@ -27,32 +28,14 @@ function SelectTags() {
     searchableTags();
   }, []);
 
-  /**
-     Component that displays search hits
-     */
-  function Hit({ hit }) {
-    function handleCallback() {
-      let tag = { name: hit.name, id: hit.objectID };
-      // if tag does not exist in list -> add to list
-      if (!displayTags.some((e) => e.name === hit.name)) {
-        /* vendors contains the element we're looking for */
-
-        setDisplayTags([...displayTags, tag]);
-        setUserTags([...userTags, tag.name]);
-      } else {
-        console.log('go here');
-        // remove from lists
-        setUserTags((current) => current.filter((o) => o !== tag.name));
-        setDisplayTags((current) => current.filter((o) => o.id !== tag.id));
-      }
-    }
-
-    return (
-      <div onClick={handleCallback}>
-        <p>{hit.name}</p>
-      </div>
-    );
-  }
+  const handleUserTags = (childData) =>{
+    setUserTags([...userTags, childData])
+    console.log(childData)
+    console.log(userTags)
+}
+const handleDisplayTags = (childData) =>{
+  setDisplayTags([...displayTags, childData]);
+}
 
   /**
    * Handles submission
@@ -61,46 +44,6 @@ function SelectTags() {
     const userRef = doc(db, 'users', currentUser.uid);
     await updateDoc(userRef, { tags: userTags });
     navigate('/home');
-  }
-
-  /* Function to not show tags when search field is empty */
-  function EmptyQueryBoundary({ children, fallback }) {
-    const { indexUiState } = useInstantSearch();
-
-    if (!indexUiState.query) {
-      return fallback;
-    }
-
-    return children;
-  }
-
-  function NoResultsBoundary({ children, fallback }) {
-    const { results } = useInstantSearch();
-
-    // The `__isArtificial` flag makes sure not to display the No Results message
-    // when no hits have been returned yet.
-    if (!results.__isArtificial && results.nbHits === 0) {
-      return (
-        <>
-          {fallback}
-          <div hidden>{children}</div>
-        </>
-      );
-    }
-
-    return children;
-  }
-
-  function NoResults() {
-    const { indexUiState } = useInstantSearch();
-
-    return (
-      <div className="noSearchResults">
-        <p>
-          Inga resultat för <q>{indexUiState.query}</q>.
-        </p>
-      </div>
-    );
   }
 
   return (
@@ -112,23 +55,10 @@ function SelectTags() {
         </p>
       </div>
       <div className="search">
-        <InstantSearch
-          searchClient={client}
-          indexName={'Tags'}
-          className="formGroup"
-        >
-          <SearchBox
-            translations={{
-              placeholder: 'Ex: Skidåkning..',
-            }}
-          />{' '}
-          {/*Sök button fix*/}
-          <EmptyQueryBoundary fallback={null}>
-            <NoResultsBoundary fallback={<NoResults />}>
-              <Hits hitComponent={Hit}></Hits>
-            </NoResultsBoundary>
-          </EmptyQueryBoundary>
-        </InstantSearch>
+        <TagSearch 
+          passUserTags={handleUserTags}
+          passDisplayTags={handleDisplayTags}
+          userTags={userTags}/>
       </div>
 
       <div className="chosenTags">
