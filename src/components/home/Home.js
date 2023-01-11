72716978b27bd58bfbc334575b28a9d262e59540
Susan Kronberg <id19skg@cs.umu.se>
2023-01-11 14:52:45 +0100

    Switchbutton functionality and random tags

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -2,34 +2,37 @@ import React from 'react';
 import { useState, useEffect } from 'react';
 import { useAuth } from '../../context/AuthContext';
 import { useNavigate } from 'react-router-dom';
-import { MdOutlineLocationOn } from 'react-icons/md';
-import CategorySection from '../atomics/CategorySection';
-import testImage from '../../ad_test.jpg';
 import Header from './Header';
 import TagSection from '../atomics/TagsSection';
 import AllCategories from './AllCategories';
-import { getAllAds } from '../../api/AdsApi';
-import { getAllTags } from '../../api/TagsApi';
-import { getUserData } from '../../api/userApi';
-import { getAdById } from '../../api/AdsApi';
+import { getAllTags, getSpecificTag } from '../../api/TagsApi';
 import SwitchButton from '../atomics/switchButton';
 
 export default function Home() {
-  const navigate = useNavigate();
-  const { currentUser } = useAuth();
-  const [ads, setAds] = useState([]);
   const [tags, setTags] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [active, setActive] = useState('adOffer');
+  const [checkoutTags, setCheckoutTags] = useState([]);
+
+  // function to get random tags. Recursive to repeat randomization in case tag alreade has been randomized. 
+  function getRandomTags(i, randomTags){
+    randomTags=[];
+    let n = tags.length;
+    while(i > 0){
+      let random = Math.floor(Math.random()*n)
+      let tag = tags[random];
+      if(randomTags.some(t => t.id == tag.id)){
+        getRandomTags(i, randomTags);
+      }else{
+        i--;
+        randomTags.push(tag);
+      }
+    }
+   return randomTags;
+  }
 
   useEffect(() => {
     try {
-      getAllAds()
-        .then((ads) => {
-          setAds(ads);
-        })
-        .catch((error) => {
-          console.log('Error', error);
-        });
 
       getAllTags()
         .then((tags) => {
@@ -38,36 +41,24 @@ export default function Home() {
         .catch((error) => {
           console.log(error);
         });
-
-      setLoading(false);
+        //getRandomTags();
+        
+        setLoading(false);
+        
     } catch (e) {
       console.log(e);
     }
   }, []);
 
-  useEffect(() => {
-    try {
-      getAllAds()
-        .then((ads) => {
-          setAds(ads);
-        })
-        .catch((error) => {
-          console.log('Error', error);
-        });
 
-      getAllTags()
-        .then((tags) => {
-          setTags(tags);
-        })
-        .catch((error) => {
-          console.log(error);
-        });
+// set random tags
+useEffect(()=>{
+  if(!loading){
+    let tags = []
+    setCheckoutTags(getRandomTags(5,tags))
+  }
+},[tags])
 
-      setLoading(false);
-    } catch (e) {
-      console.log(e);
-    }
-  }, []);
 
   const switchData = {
     buttonOne: 'Erbjudna',
@@ -77,14 +68,19 @@ export default function Home() {
     activeButton: 'buttonOne',
   };
 
+ 
+  function updateActive(data){
+    setActive(data);
+  }
+
   return (
     <div className="homePage">
       <Header></Header>
-      <SwitchButton data={switchData} />
+      <SwitchButton updateActive={updateActive} data={switchData} />
       {!loading && (
         <div className="mainContent">
-          <TagSection sectionTitle="Kolla in dessa" tags={tags}></TagSection>
-          <AllCategories ads={ads} categories={tags}></AllCategories>
+          <TagSection sectionTitle="Kolla in dessa" tags={checkoutTags}></TagSection>
+          <AllCategories active={active} categories={tags}></AllCategories>
         </div>
       )}
     </div>
